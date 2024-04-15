c6918dfe010eb2e4e24f0eb431332a2b2bc6c805
Nhu Bao <tnbao1009@gmail.com>
2024-04-15 19:39:51 +0700

    new code

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 
-function sum2Num(a,b){
-    return a+b;
-}
+import { sum2Num, printConsole } from "./util.js"; 
+import sub2Num from "./util.js"; // Default-C1
+import {default as sub2Num} from "./ultil.js"; // Default-C2
 
 function main(){
     console.log("a + b = ",sum2Num(1,2));
+    printConsole();
+    console.log("a - b = ",sub2Num(1,2));
 }
 
 main();
---
+++ src/util.js
@@ -0,0 +1,12 @@
+
+export function sum2Num(a,b){
+    return a+b;
+}
+
+export function printConsole(){
+    console.log('message');
+}
+
+export default function sub2Num(numA, numB){
+    return numA - numB;
+}
